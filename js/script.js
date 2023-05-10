ae8946f7ee1fa81a8e26eae575dafdbcf1cf6a3d
jramma <jrmasip97@gmail.com>
2023-05-10 19:55:31 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-
-};
-
-const sr = ScrollReveal({
-    distance: '65px',
-    duration: 2600,
-    delay:450,
-    reset: true
-});
-
-sr.reveal('.hero-text',{delay:200,origin:'top'});
-sr.reveal('.hero-img',{delay:450,origin:'top'});
-sr.reveal('.icons',{delay:500,origin:'left'});
-sr.reveal('.scroll-down',{delay:500,origin:'left'});
-
-
 function randomBg() {
     var random = Math.floor(Math.random() * 4) + 1; // Genera un número aleatorio entre 1 y 4
     document.body.className = 'bg-image-' + random; // Asigna la clase correspondiente a la imagen de fondo
-  }
-  
+}
